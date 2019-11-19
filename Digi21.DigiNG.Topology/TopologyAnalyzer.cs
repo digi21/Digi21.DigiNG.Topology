8a2d7a55087e94ed6727d6ef6efc84da3899b4c9
José Ángel Martínez Torres <joseangelmt@outlook.com>
2019-11-19 07:32:58 +0100

    Deleted MarshalAs for booleans

--- Digi21.DigiNG.Topology/TopologyAnalyzer.cs
+++ Digi21.DigiNG.Topology/TopologyAnalyzer.cs
@@ -1,7 +1,6 @@
 ﻿using Digi21.DigiNG.Entities;
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace Digi21.DigiNG.Topology
 {
@@ -11,8 +10,8 @@ namespace Digi21.DigiNG.Topology
         public static void AnalyzeCentroids(this IEnumerable<ClosedLoopWithHoles> polygons, IEnumerable<ReadOnlyText> centroids, Action<int> progressChanged) { throw null; }
         public static void AnalyzeCentroids(this IEnumerable<ClosedLoopWithHoles> polygons, IEnumerable<ReadOnlyText> centroids, Action<ClosedLoopWithHoles, IEnumerable<ReadOnlyText>> duplicatedCentroids) { throw null; }
         public static void AnalyzeCentroids(this IEnumerable<ClosedLoopWithHoles> polygons, IEnumerable<ReadOnlyText> centroids, Action<ClosedLoopWithHoles, IEnumerable<ReadOnlyText>> duplicatedCentroids, Action<int> progressChanged) { throw null; }
-        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, [MarshalAs(UnmanagedType.U1)] bool analyzePolygonRelationship) { throw null; }
-        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, [MarshalAs(UnmanagedType.U1)] bool analyzePolygonRelationship, Action<int> progressChanged) { throw null; }
-        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, [MarshalAs(UnmanagedType.U1)] bool analyzePolygonRelationship, Action<int> progressChanged, Action<ReadOnlyLine> lineWithDoublePoints, Action<Entity> entityWithOnlyOnePoint, Action<IEnumerable<ReadOnlyLine>> openPolygon) { throw null; }
+        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, bool analyzePolygonRelationship) { throw null; }
+        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, bool analyzePolygonRelationship, Action<int> progressChanged) { throw null; }
+        public static IEnumerable<ClosedLoopWithHoles> AnalyzeTopology(this IEnumerable<Entity> entities, bool analyzePolygonRelationship, Action<int> progressChanged, Action<ReadOnlyLine> lineWithDoublePoints, Action<Entity> entityWithOnlyOnePoint, Action<IEnumerable<ReadOnlyLine>> openPolygon) { throw null; }
     }
 }
